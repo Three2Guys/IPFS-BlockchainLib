6d4a5cfd28656a8bf52239c46964e640dc3c9ef4
Fred25022004 <fkung9104@gmail.com>
2024-11-04 20:24:15 +0800

    fixed the problem of not showing cid on clicking upload btn in console

--- src/components/BookUpload.js
+++ src/components/BookUpload.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 
+const uploadFile = async (file) => {
+  if (file) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch("http://localhost:5001/api/v0/add", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    console.log("File uploaded to IPFS with CID:", data.Hash);
+  } else {
+    console.log("No file selected");
+  }
+};
+
 const BookUpload = () => {
   const [file, setFile] = useState(null);
-
-  const uploadFile = async () => {
-    if (file) {
-      const formData = new FormData();
-      formData.append("file", file);
-
-      const response = await fetch("https://ipfs.infura.io:5001/api/v0/add", {
-        method: "POST",
-        body: formData,
-      });
-
-      const data = await response.json();
-      console.log("File uploaded to IPFS with CID:", data.Hash);
-    } else {
-      console.log("No file selected");
-    }
-  };
 
   return (
     <div>
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-      <button onClick={uploadFile}>Upload</button>
+      <button onClick={() => uploadFile(file)}>Upload</button>
     </div>
   );
 };
